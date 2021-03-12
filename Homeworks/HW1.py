76a05a977833ccee1bd73ba5c9ad64797425f395
Basak4 <76090724+Basak4@users.noreply.github.com>
2021-03-13 00:36:24 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,14 @@
-#Explain your work
+#HMW1
 
-#Question 1
-for x in range(a):
-	print(a)
+lastNum = int(input("Please enter the final number "))
+allNums = list(range(lastNum))
+oddNums = [i for i in allNums if i % 2 != 0]
+evenNums = [i for i in allNums if i % 2 == 0]
+print("Odd numbers until ", lastNum, " :", oddNums)
+print("Even numbers until ", lastNum, " :", evenNums)
+oddNums.extend(evenNums)
+print("Merge odd numbers and even numbers :", oddNums)
+# oddNums.sort()  # to make more understandable
+finalResult = [num1 * 2 for num1 in oddNums]
+print("Multiply all values by 2 :", finalResult)
+
