759c3613d6e87f186a20c73b25bbbf2c377045ef
Basak4 <76090724+Basak4@users.noreply.github.com>
2021-03-13 01:29:31 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,38 @@
-#Explain your work
+#HMW2
 
-#Question 1
-for x in range(a):
-	print(a)
+CV1 = {
+    "name": (input("Please enter your name ")),
+    "surname": (input("Please enter your surname ")),
+    "birth year": int(input("Please enter your birth year ")),
+    "job": (input("Please enter your job ")),
+}
+CV2 = {
+    "name": (input("Please enter your name ")),
+    "surname": (input("Please enter your surname ")),
+    "birth year": int(input("Please enter your birth year ")),
+    "job": (input("Please enter your job ")),
+}
+CV3 = {
+    "name": (input("Please enter your name ")),
+    "surname": (input("Please enter your surname ")),
+    "birth year": int(input("Please enter your birth year ")),
+    "job": (input("Please enter your job ")),
+}
+CV4 = {
+    "name": (input("Please enter your name ")),
+    "surname": (input("Please enter your surname ")),
+    "birth year": int(input("Please enter your birth year ")),
+    "job": (input("Please enter your job ")),
+}
+CV5 = {
+    "name": (input("Please enter your name ")),
+    "surname": (input("Please enter your surname ")),
+    "birth year": int(input("Please enter your birth year ")),
+    "job": (input("Please enter your job ")),
+}
+
+print("CV for the first person : ", CV1)
+print("CV for the second person : ", CV2)
+print("CV for the third person : ", CV3)
+print("CV for the fourth person : ", CV4)
+print("CV for the fifth person : ", CV5)
