86bbb3c2df7bcdca69b1f87ebe5f17526894f683
Basak4 <76090724+Basak4@users.noreply.github.com>
2021-03-14 23:58:42 +0300

    Update Final_Project.py

--- Final Project/Final_Project.py
+++ Final Project/Final_Project.py
@@ -1,3 +1,24 @@
-import pandas as pd
+print("Welcome to The Best Knowledge Competition Program")
+count = 0
 
-data = pd.read_csv("project.csv")
+print("First question : (answer is one")
+answer1 = int(input("Please enter your answer "))
+if answer1 == 'one':
+    count = count+1
+print("Second question : (answer is two")
+answer2 = int(input("Please enter your answer "))
+if answer2 == 'two':
+    count = count+1
+print("Third question : (answer is three")
+answer3 = int(input("Please enter your answer "))
+if answer3 == 'three':
+    count = count+1
+print("Fourth question : (answer is four")
+answer4 = int(input("Please enter your answer "))
+if answer4 == 'four':
+    count = count+1
+print("Fifth question : (answer is five")
+answer5 = int(input("Please enter your answer "))
+if answer5 == 'five':
+    count = count + 1
+    
